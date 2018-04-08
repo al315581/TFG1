91f1e85afe17609a505a25d2ce0f979570483d63
al315581 <al315581@uji.es>
2018-04-08 17:50:17 +0200

    Initial project

---
+++ tfg1/Assets/Scripts/BallScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    public bool playerNear = false;
+    public bool ballStopped;
+    public float velocity = 1f;
+
+    private Transform wallHitPoint; //With raycast we will see the point where the ball will collide;
+    int wallMask;
+    Vector3 direction;
+
+    void Awake()
+    {
+        wallMask = LayerMask.GetMask("Walls");
+    }
+
+	void Start () {
+        ballStopped = true;
+        direction = transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void FixedUpdate()
+    {
+        direction = Vector3.Normalize(transform.forward);
+        //direction = Vector3.Normalize(transform.forward);
+        if(ballStopped == false)
+        {
+            //It works this way, but Vector3.forward is (0,0,1)... WHY????????????????????
+            //transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+            transform.Translate(direction * velocity * Time.deltaTime, Space.World);
+           
+        }
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            print("el jugador está cerca");
+            playerNear = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("el jugador sale");
+            playerNear = false;
+        }
+    }
+
+ 
+
+    public void changeDirection(Quaternion _rotation)
+    {
+        transform.rotation = _rotation;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position, transform.position + direction * 2);
+    }
+
+
+}
---
+++ tfg1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody playerRB;
+    Vector3 movement;
+    Vector3 rotation;
+    public int speed;
+    private float lastV, lastH;
+    public float slerpSpeed;
+
+    public BallScript ballSript;
+    public GameObject ball;
+    Vector3 direction;
+
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+       // direction = transform.forward;
+
+    }
+
+    void Start()
+    {
+        direction = transform.forward;
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+
+        if(h==0 && v == 0)
+        {
+            //Rotate(lastH, lastV);
+        }
+        else
+        {
+            Rotate(h, v);
+            lastH = h;
+            lastV = v;
+        }
+
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRB.MovePosition(transform.position + movement);
+    }
+
+    private void Rotate(float h, float v)
+    {
+        rotation.Set(h, 0f, v);
+        Quaternion newRotation = Quaternion.LookRotation(rotation);
+        playerRB.rotation = Quaternion.Slerp(playerRB.rotation, newRotation, slerpSpeed * Time.deltaTime);
+        direction = transform.forward;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            if (ballSript.playerNear)
+            {
+                //ball.transform.rotation = transform.rotation;
+                ballSript.changeDirection(transform.rotation);
+                ballSript.ballStopped = false;
+            }
+        }
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + direction * 2);
+    }
+}
