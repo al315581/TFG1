eb519d1fb810ec73c61008bb3335cc795c9bfc1e
al315581 <al315581@uji.es>
2018-04-16 04:22:13 +0200

    Añadidos SkyBoxes

---
+++ tfg1/Assets/Scripts/SkyBox/SkyboxChanger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SkyboxChanger : MonoBehaviour
+{
+    public Material[] Skyboxes;
+    private Dropdown _dropdown;
+    public int skybox = 0;
+
+    public void Awake()
+    {
+       // _dropdown = GetComponent<Dropdown>();
+        //var options = Skyboxes.Select(skybox => skybox.name).ToList();
+        //_dropdown.AddOptions(options);
+    }
+
+    public void Start()
+    {
+        RenderSettings.skybox = Skyboxes[skybox];
+    }
+
+    public void ChangeSkybox()
+    {
+        if(skybox < Skyboxes.Length)
+        {
+            skybox += 1;
+        }
+        else
+        {
+            skybox = 0;
+        }
+
+        RenderSettings.skybox = Skyboxes[skybox];
+        RenderSettings.skybox.SetFloat("_Rotation", 10);
+
+    }
+
+}
