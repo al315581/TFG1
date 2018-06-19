25d1021fc9922a0975f4e0f2c5eb62726375daa6
al315581 <al315581@uji.es>
2018-06-19 14:11:52 +0200

    Sin cambios

---
+++ tfg1/Assets/InitialCinematic/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform target;
+    public float velocityRotation = 1f;
+    public float velocityUp = 1f;
+    public float velocityBack = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+        transform.Translate(Vector3.right * Time.deltaTime * velocityRotation);
+        target.Translate(Vector3.up * Time.deltaTime * velocityUp);
+        transform.Translate(Vector3.up * Time.deltaTime * velocityUp);
+        transform.Translate(Vector3.back * Time.deltaTime * velocityBack);
+	}
+}
---
+++ tfg1/Assets/InitialCinematic/Cinematic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cinematic : MonoBehaviour {
+    public Material characterMaterial;
+    public Material[] materials;
+    // Use this for initialization
+    public float fadeValue;
+    public float maxValue = 4f;
+    public float velocity = 1f;
+	void Start () {
+        characterMaterial.SetFloat("Vector1_A235A670", -3);
+        fadeValue = -3;
+        StartCoroutine(characterAppearance());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator characterAppearance()
+    {
+        while(fadeValue < maxValue)
+        {
+            fadeValue += Time.deltaTime * velocity;
+            //characterMaterial.SetFloat("Vector1_A235A670", fadeValue);
+            for(int i = 0; i< materials.Length; i++)
+            {
+                materials[i].SetFloat("Vector1_A235A670", fadeValue);
+            }
+            yield return null;
+        }
+
+        yield return null;
+    }
+}
