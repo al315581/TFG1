644273566ce3e0dee62d1ef5b80977bd0c21ff22
al315581 <al315581@uji.es>
2018-06-28 19:20:32 +0200

    Añadidos los botones en title.

---
+++ tfg1/Assets/Scripts/TigleScene/ButtonManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+    public Button m_YourButton, m_YourSecondButton;
+
+
+    // Use this for initialization
+    void Start () {
+        Button btn = m_YourButton.GetComponent<Button>();
+        Button btn2 = m_YourSecondButton.GetComponent<Button>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void PlayButtonPressed()
+    {
+        print("PLAY");
+        GameObject.FindObjectOfType<LevelChanger>().FadeToNextLevel();
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+}
